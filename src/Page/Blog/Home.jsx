ac7f0d0238d9a7e6e7cd4e37705b72fc837ee564
Saroj Tamang <sartamang234@gmail.com>
2025-07-26 21:56:04 +0545

    2

--- src/Page/Blog/Home.jsx
+++ src/Page/Blog/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Layout from '../../Components/Layout/Layout'
+import Card from './Components/Card'
+
 
 const Home = () => {
   return (
     <>
-        <h1>Home page ho yo</h1>
+        <Layout>
+            <div className='flex flex-wrap justify-center gap-5 m-5'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </Layout>
     </>
   )
 }
